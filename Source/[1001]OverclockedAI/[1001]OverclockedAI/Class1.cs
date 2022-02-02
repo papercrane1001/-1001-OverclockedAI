3757d2317a767cbcfcc36ced9afbcb0c02591c56
papercrane1001 <papercrane1001@gmail.com>
2022-02-02 07:16:12 -0800

    Made the textures.  Forgot to export them as png's

--- Source/[1001]OverclockedAI/[1001]OverclockedAI/Class1.cs
+++ Source/[1001]OverclockedAI/[1001]OverclockedAI/Class1.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Verse;
+using RimWorld;
+
 namespace _1001_OverclockedAI
 {
-    public class Class1
+    public class CompProperties_AdjustableFacility : CompProperties_Facility
     {
+        public int step = 1;
+
+        public CompProperties_AdjustableFacility()
+        {
+            this.compClass = typeof(CompFacility);
+            this.maxDistance = 16f;
+            //if(this.statOffsets.Contains())
+            //if(this.statOffsets.Where(x => x.stat == StatDefOf.WorkTableWorkSpeedFactor)
+            StatModifier s = new StatModifier();
+            s.stat = StatDefOf.WorkTableWorkSpeedFactor;
+            this.statOffsets.Add(s);
+        }
+
+        public void AdjustWorkSpeedFactor(float newVal)
+        {
+            StatModifier s = new StatModifier();
+            s.stat = StatDefOf.WorkTableWorkSpeedFactor;
+            var ls = statOffsets.Where(x => x.stat == s.stat);
+
+            if(ls.Count() > 0)
+            {
+                statOffsets.Remove(ls.FirstOrDefault());
+                ls.FirstOrDefault().value = newVal;
+                statOffsets.Add(ls.FirstOrDefault());
+            }            
+            
+        }
     }
+
+    //public class CompProperties_
 }
